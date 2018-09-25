d81eb049936758cf52b096d9125cebc0350867e3
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-09-25 13:08:20 -0700

    setup blog form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    render(){
-        return(
-            <div>
-                PostsNew!
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
             </div>
+        )
+    }
+
+
+
+    render() {
+        return (
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                
+                />
+            </form>
         )
     }
 }
 
-export default PostsNew
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew)
